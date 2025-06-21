2c4707045959cb5b80d9c8432b2013232318c3d4
abhilashMirupati <abhilash.reddy7691@gmail.com>
2025-06-21 18:48:56 -0400

    Update logger.py

--- utils/utils/logger.py
+++ utils/utils/logger.py
@@ -1,13 +1,16 @@
 import logging
-import sys
+from typing import Optional
 
-def setup_logger(name: str, level: int = logging.INFO) -> logging.Logger:
+def setup_logger(name: Optional[str] = None) -> logging.Logger:
+    """
+    Sets up and returns a logger for the given name.
+    Ensures consistent logging formatting for the entire framework.
+    """
     logger = logging.getLogger(name)
-    logger.setLevel(level)
-    logger.propagate = False
-    if not logger.handlers:
-        handler = logging.StreamHandler(sys.stdout)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    if not logger.hasHandlers():
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s %(levelname)s [%(name)s]: %(message)s')
         handler.setFormatter(formatter)
         logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
     return logger
